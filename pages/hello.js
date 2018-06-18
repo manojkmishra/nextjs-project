9a3e75737cfe89d05a420a2b45790094f878a5c6
manojkmishra <manojmishra571980@gmail.com>
2018-06-18 16:33:24 +1000

    link for world page added in hello page

--- pages/hello.js
+++ pages/hello.js
@@ -1,1 +1,3 @@
-export default () => <div> hello page </div>;
+//export default () => <div> hello page </div>;
+import Link from "next/link";
+export default () => <Link href="/world">[This is ]hello page]-> go to world page</Link>;
